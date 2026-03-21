5ed1989ff3aeedcbd23367be4b5bbca7dc2e5339
Ralph Daou <ralph.daou5@gmail.com>
2026-03-21 16:40:19 +0200

    Quiz file created

---
+++ Quiz.js
@@ -0,0 +1,11 @@
+const test = new Map();
+test.add("What is the largest country in the world?",["Russia","USA","Germany","China"]);
+test.add("What is the most famous language?",["Chinese","Arabic","Japanese","English"]);
+test.add("World cup 2010 winner?",["Brasil","Italy","Spain","Argentina"]);
+
+const ans = ["Russia","English","Spain"];
+
+
+function start_quiz(){
+    document.getElementById('greeting-form').style.display = 'none';
+}
